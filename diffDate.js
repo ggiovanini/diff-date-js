f1e2bc1c1631aea833c0def4c95fc2a472283b93
Giuliano Giovanini <gil@uparte.com.br>
2019-06-06 13:39:44 -0300

    Implemented regressive counter, fix bugs

--- diffDate.js
+++ diffDate.js
@@ -2,6 +2,7 @@ function DiffDate(glue = ':') {
     const self = this;
     const dataRequire = 'data-date-diff';
     const separator = glue;
+    const negative = '-';
 
     window.addEventListener('load', function () {
         self.init();
@@ -58,21 +59,52 @@ function DiffDate(glue = ':') {
         const pastDate = new Date(date1);
         const today = new Date();
         const glue = elem.dataset.dateGlue || separator;
-        elem.innerHTML = self.daysBetween(pastDate, today, glue);
+        const minus = elem.dataset.dateMinus || negative;
+        const stop = elem.dataset.dateZero && elem.dataset.dateZero === 'stop';
+
+        const difference = Math.round(
+            (pastDate.getTime() - today.getTime()) / 1000
+        );
+
+        if (difference <= 0 && stop) {
+            elem.innerHTML = `0${glue}00`;
+            self.stop(elem);
+            return;
+        }
+
+        if (difference) {
+            elem.innerHTML = self.daysBetween(pastDate, today, glue, minus);
+            if (stop && (difference === -0 || difference === 0)) {
+                self.stop(elem);
+            }
+            return;
+        }
+
+        elem.innerHTML = self.daysBetween(today, pastDate, glue, minus, stop);
+        if (stop && (difference === -0 || difference === 0)) {
+            self.stop(elem);
+        }
     };
 
-    this.daysBetween = function (date1, date2, glue = separator) {
+    this.daysBetween = function (date1, date2, glue = separator, minus = negative) {
         const difference = Math.round(
             (date2.getTime() - date1.getTime()) / 1000
         );
 
-        const minutes = Math.floor(difference / 60);
-        const seconds = difference - minutes  * 60;
-        if (seconds < 10) {
-            return (`${minutes}${glue}0${seconds}`);
+        let minutes = Math.floor(difference / 60);
+        let seconds = difference - minutes  * 60;
+        let setNegative = '';
+        if (difference < 0) {
+            minutes = (minutes+1) * -1;
+            seconds = 60 - seconds;
+            setNegative = minus === 'false' ? '' : minus;
         }
 
-        return (`${minutes}${glue}${seconds}`);
+        if (seconds < 10) {
+            return (`${setNegative}${minutes}${glue}0${seconds}`);
+        }
+
+        return (`${setNegative}${minutes}${glue}${seconds}`);
     };
 
 }
